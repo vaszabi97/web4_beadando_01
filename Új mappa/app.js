63a775f92a6dcf2af023e36fc1f6d189fcfdca9c
vaszabi97 <vaszabi97@gmail.com>
2020-11-01 16:42:54 +0100

    WEB4 beadandó

---
+++ Új mappa/app.js
@@ -0,0 +1,4 @@
+const btnScrollOptionStart = document.getElementById('btn-scroll-start');
+const btnScrollOptionEnd = document.getElementById('btn-scroll-end');
+const btnScrollOptionPrev = document.getElementById('btn-scroll-prev');
+const btnScrollOptionNext = document.getElementById('btn-scroll-next');
